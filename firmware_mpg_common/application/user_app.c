054c1d6a59f72aaa8089d861529580715423ab8a
Weifeng1 <350121791@qq.com>
2016-06-14 16:46:19 +0800

    my_project_part2

--- firmware_mpg_common/application/user_app.c
+++ firmware_mpg_common/application/user_app.c
@@ -146,23 +146,59 @@ State Machine Function Definitions
 /* Wait for a message to be queued */
 static void UserAppSM_Idle(void)
 {
-  static u8 counter0=0;
+  static u8 counter=0;
+  static u16 counter0=0;
   static u8 counter1=0;
   static u8 flag=FALSE;
   static u8 flag1=FALSE;
   static u8 flag2=TRUE;
   static u8 flag3=FALSE;
-  static u8 DelayTime=500;
-  static u8 UserApp_CursorPosition = LINE2_END_ADDR;
+  static u16 DelayTime;
+  
+  static u8 number[100];
+  static u32 Randomnumber=1;
+  static u32 Midnumber;
+  static u8 counter2=0;
+  static u8 counter3=0;
+  static u8 UserApp_CursorPosition1[100];
+  static u8 UserApp_CursorPosition2[100];
+  
+  static u8 BrickLine=0;
+  //
+  static u16 delaytimecounter=0;
+  static u8 u8buzzerrateIndex=0;
+  static u16 u16buzzerrate[]={330,294,262,294,330,330,330,0,
+                              294,294,294,0,
+                              330,392,392,0,
+                              330,294,262,294,330,330,330,0,
+                              330,294,294,330,294,262,0};
+  
+  /*delaytimecounter++;
+ 
+  if(delaytimecounter==200)
+  {
+    delaytimecounter=0;
+    u8buzzerrateIndex++;
+
+    PWMAudioSetFrequency(BUZZER1,u16buzzerrate[u8buzzerrateIndex]);
+    PWMAudioOn(BUZZER1);
+
+        
+    if(u8buzzerrateIndex==31)
+    {
+      u8buzzerrateIndex=0;
+    } 
+  }*/
+  //
   if(WasButtonPressed(BUTTON0))
   {
     ButtonAcknowledge(BUTTON0);
     LCDCommand(LCD_CLEAR_CMD); 
     LCDMessage(LINE2_START_ADDR+2, "8");
+    //flag3=TRUE;
     flag=TRUE;
     LedOff(RED);
     PWMAudioOff(BUZZER1);
-    UserApp_CursorPosition=LINE2_END_ADDR;
     flag1=FALSE;
     flag2=TRUE;
     DelayTime=500;
@@ -174,6 +210,17 @@ static void UserAppSM_Idle(void)
     if(counter0==DelayTime)
     {
       counter0=0;
+      counter1++;
+      counter++;
+      if (counter==20)
+      {
+        counter=0;
+        if(DelayTime>50)
+        {
+          DelayTime=DelayTime-10;
+        }
+      }
+     
       /*Contral the "man" up or down*/
       LCDCommand(LCD_CLEAR_CMD);
       if(flag1)
@@ -184,22 +231,57 @@ static void UserAppSM_Idle(void)
       {
         LCDMessage(LINE2_START_ADDR+2, "8");
       }
-      LCDMessage(UserApp_CursorPosition, "0");
-      UserApp_CursorPosition--;
-      if (UserApp_CursorPosition+1 == LINE2_START_ADDR)
+      if(counter1==3)
       {
-        
-        if(DelayTime>50)
+        counter1=0;
+       
+        Midnumber=Randomnumber*22695477+1;
+        Randomnumber=(Midnumber/65535)&0x7fff;
+        DebugPrintNumber(Randomnumber);
+        DebugPrintf("  ");
+        /*shot a brick randomly*/
+        if(Randomnumber%2==0)
         {
-          DelayTime=DelayTime-10;
+           
+          BrickLine=1;
+          UserApp_CursorPosition1[counter2]=19;
+          LCDMessage(LINE1_START_ADDR+UserApp_CursorPosition1[counter2], "0");
+          counter2++;
         }
-        UserApp_CursorPosition = LINE2_END_ADDR;
+        else if(Randomnumber%2!=0)
+        {
+          
+          BrickLine=2;
+          UserApp_CursorPosition2[counter3]=19;
+          LCDMessage(LINE2_START_ADDR+UserApp_CursorPosition2[counter3], "0");
+          counter3++;
+        }        
       }
-      
+      for(u8 m=0;m<counter2;m++)
+      {
+        UserApp_CursorPosition1[m]--;
+        LCDMessage(LINE1_START_ADDR+UserApp_CursorPosition1[m], "0");
+      }
+      for(u8 n=0;n<counter3;n++)
+      {
+        UserApp_CursorPosition2[n]--;
+        LCDMessage(LINE2_START_ADDR+UserApp_CursorPosition2[n], "0");
+      }
+      /*if(BrickLine==1)
+      {
+        LCDMessage(LINE1_START_ADDR+UserApp_CursorPosition1[counter2], "0"); 
+        UserApp_CursorPosition1[counter2]--;  
+      }
+      if(BrickLine==2)
+      {
+        LCDMessage(LINE2_START_ADDR+UserApp_CursorPosition2[counter3], "0"); 
+        UserApp_CursorPosition2[counter3]--;  
+      }*/
     }
     if(WasButtonPressed(BUTTON1))
     {
       ButtonAcknowledge(BUTTON1);
+      //LCDCommand(LCD_CLEAR_CMD);
       flag1=TRUE;
       flag2=FALSE;
       flag3=FALSE;
@@ -208,26 +290,29 @@ static void UserAppSM_Idle(void)
     if(WasButtonPressed(BUTTON2))
     {
       ButtonAcknowledge(BUTTON2);
+      //LCDCommand(LCD_CLEAR_CMD);
       flag1=FALSE;
       flag2=TRUE;
       flag3=TRUE;
       LCDMessage(LINE2_START_ADDR+2, "8"); 
     }
     
-    if(UserApp_CursorPosition+1==LINE2_START_ADDR+2)
+    /*if(UserApp_CursorPosition[j]+1==LINE2_START_ADDR+2)
     {
       if(flag3)
       {
+        
         flag3=FALSE;
         flag=FALSE;
         LedOn(RED);
+        
         PWMAudioOn(BUZZER1);
         LCDCommand(LCD_CLEAR_CMD);
         LCDMessage(LINE1_START_ADDR+5, "Game over!"); 
         LCDMessage(LINE2_START_ADDR, "Press BUTTON0 start");
       }
-    }
-  }  
+    }*/
+  }
 } /* end UserAppSM_Idle() */
      
 
